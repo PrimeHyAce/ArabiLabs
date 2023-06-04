ebe6d69152ef478efad20a4250dd1c48f6add4a7
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-06-04 14:59:09 +0700

    add quiz

---
+++ Assets/QuizManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using RTLTMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text questionText;
+    [SerializeField] Image questionImage;
+    [SerializeField] List<RTLTextMeshPro> answerTexts;
+
+    [SerializeField] Slider quizProgressSlider;
+    [SerializeField] GameObject quizResultPanel;
+    [SerializeField] TMP_Text quizResultText;
+
+    [SerializeField] List<QuizData> quizDatas;
+
+    int currentQuestionIndex = 0;
+    int score = 0;
+
+    private void Start()
+    {
+        SetQuestion();
+    }
+
+    private void Update()
+    {
+        quizProgressSlider.value = (float)currentQuestionIndex / quizDatas.Count;
+    }
+
+    void SetQuestion()
+    {
+        questionText.text = quizDatas[currentQuestionIndex].question;
+        questionImage.sprite = quizDatas[currentQuestionIndex].questionImage;
+
+        for (int i = 0; i < answerTexts.Count; i++)
+        {
+            answerTexts[i].text = quizDatas[currentQuestionIndex].answers[i];
+        }
+    }
+
+    public void CheckAnswer(int answerIndex)
+    {
+        if (answerIndex == quizDatas[currentQuestionIndex].correctAnswerIndex)
+        {
+            Debug.Log("Correct");
+            score++;
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+        if (currentQuestionIndex >= quizDatas.Count - 1)
+        {
+            Debug.Log("Quiz Finished");
+            quizResultPanel.SetActive(true);
+            quizResultText.text = "Score: " + score * 20;
+        }
+        else
+        {
+            currentQuestionIndex++;
+            SetQuestion();
+        }
+
+        // currentQuestionIndex++;
+
+        // if (currentQuestionIndex >= quizDatas.Count)
+        // {
+        //     // currentQuestionIndex = 0;
+        //     Debug.Log("Quiz Finished");
+        //     quizResultPanel.SetActive(true);
+        //     quizResultText.text = "Quiz Finished";
+        // }
+
+        // SetQuestion();
+    }
+}
+
+[System.Serializable]
+public class QuizData
+{
+    public string question;
+    public Sprite questionImage;
+    public List<string> answers;
+    public int correctAnswerIndex;
+}
